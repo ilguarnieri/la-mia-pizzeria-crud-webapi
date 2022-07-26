bcd609621706476040883a08d1fb11c7638ff6ca
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-07-26 16:43:46 +0200

    insert login register

--- Data/PizzaContext.cs
+++ Data/PizzaContext.cs
@@ -1,11 +1,20 @@
 ﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace la_mia_pizzeria_static.Data
 {
-    public class PizzaContext : DbContext
+    public class PizzaContext : IdentityDbContext<IdentityUser>
     {
+        public PizzaContext(DbContextOptions<PizzaContext> options) : base(options)
+        {
 
+        }
+        public PizzaContext()
+        {
+
+        }
         public DbSet<Pizza> Pizzas { get; set; }
         public DbSet<Category> Categories { get; set; } 
 
