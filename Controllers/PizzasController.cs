b2034794f2f825f02d3bb3c5ee920448378341bc
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-07-26 12:10:48 +0200

    create api

---
+++ Controllers/PizzasController.cs
@@ -0,0 +1,24 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    [Route("api/pizzas")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetPizzas()
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                IEnumerable<Pizza> pizzas = db.Pizzas.ToList();
+
+                return Ok(pizzas);
+            }
+        }
+    }
+}
